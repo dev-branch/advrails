19e2e9efe5c25527323f61ebcb8025abefa3af35
Chyld Medford <chyld.medford@gmail.com>
2017-09-02 13:11:30 -0700

    phase 2

--- logger/scripts/start-dev.sh
+++ logger/scripts/start-dev.sh
@@ -8,12 +8,7 @@ until psql -h pg -U postgres -c "\l"; do
   sleep 1
 done
 
-until redis-cli -h redis -r 1 KEYS test; do
-  >&2 echo "Redis is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres & Redis are up - executing command(s)"
+>&2 echo "Postgres is up - executing command(s)"
 rails db:create
 rails db:migrate
 rails db:seed
